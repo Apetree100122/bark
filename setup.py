7abdab8ddacb4621777b3d562817ea30e51c4627
Alexander  Petree <apetree1001@email.phoenix.edu>
2023-10-03 23:08:43 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
-from setuptools import setup
+"""A setuptools based setup module.
+See: <https://packaging.python.org/en/latest/distributing.html>
+"""
 
-setup()
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="example_pkg",
+    version="0.1",
+    author_email="apetree1001@email.phoenix.edu",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
